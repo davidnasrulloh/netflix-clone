ef97a71eaeff06ea50c7e3d30e8bcc544be2c6ea
David Nasrulloh <davidkrb52@gmail.com>
2023-04-07 23:51:15 +0700

    feat/david: setting auth login and register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,18 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router"
+
+import {FcGoogle} from "react-icons/fc";
+import {FaGithub} from "react-icons/fa";
 
 const Auth = () => {
 
+    const router = useRouter();
+
     const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
     const [variant, setVariant] = useState('login');
@@ -13,6 +21,34 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     },[])
 
+    const login = useCallback( async ()=>{
+        try {
+            await signIn('credentials',{
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+        } catch (error) {
+            console.log(error)
+        }
+    },[email, password, router]);
+
+    const register = useCallback(async ()=> {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+
+            login();
+        } catch (error) {
+            console.error(error)
+        }
+    }, [email, name, password, login]);
 
     return(
         <div className="relative w-full h-full lg:bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -27,10 +63,10 @@ const Auth = () => {
                             {variant === 'register' && (
                                 <Input
                                     label="Username"
-                                    onChange={(ev: any) => setUsername(ev.target.value)}
+                                    onChange={(ev: any) => setName(ev.target.value)}
                                     id="username"
                                     type="text"
-                                    value={username}
+                                    value={name}
                                 />
                             )}
                             <Input
@@ -45,12 +81,20 @@ const Auth = () => {
                                 onChange={(ev: any) => setPassword(ev.target.value)}
                                 id="password"
                                 type="password"
-                                value={password}
+                                value={password}    
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30}/>
+                            </div>
+                            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={30}/>
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                             
