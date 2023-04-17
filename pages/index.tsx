64a55501adcf5d05d3ee82876abf51c07f8e93a6
David Nasrulloh <davidkrb52@gmail.com>
2023-04-18 03:00:52 +0700

    feat/david: update login and logout and protected route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
+import { NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+
+export async function getServerSideProps (context: NextPageContext){
+  const session = await getSession(context);
+  if(!session){
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+  
+  return{
+    props: {}
+  }
+}
+
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl font-bold text-white">Netflix clone</h1>
+      <button className="h-10 w-full bg-white" onClick={()=>signOut()}>Logout</button>
     </>
   )
 }
