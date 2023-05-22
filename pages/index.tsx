5e2177a5ee8c5fb7b09b6d666378e688b542d73b
David Nasrulloh <davidkrb52@gmail.com>
2023-05-22 21:20:40 +0700

    feat/david: profile screen and navbar component 10%

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
+import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps (context: NextPageContext){
   const session = await getSession(context);
@@ -18,9 +20,14 @@ export async function getServerSideProps (context: NextPageContext){
 }
 
 export default function Home() {
+
+  const { data: user } = useCurrentUser()
+
   return (
     <>
+      <Navbar/>
       <h1 className="text-2xl font-bold text-white">Netflix clone</h1>
+      <p className="text-white">Logged in as {user?.email}</p>
       <button className="h-10 w-full bg-white" onClick={()=>signOut()}>Logout</button>
     </>
   )
