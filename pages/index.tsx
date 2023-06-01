3fea0161ff0f44d17729705de2e0efb99d57a620
David Nasrulloh <davidkrb52@gmail.com>
2023-06-01 17:07:50 +0700

    add my list but still there bug in add favorite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
-import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from './../components/MovieList';
 import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 export async function getServerSideProps (context: NextPageContext){
   const session = await getSession(context);
@@ -24,8 +24,8 @@ export async function getServerSideProps (context: NextPageContext){
 
 export default function Home() {
 
-  // const { data: user } = useCurrentUser()
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = []} = useFavorites()
 
   return (
     <>
@@ -33,6 +33,7 @@ export default function Home() {
       <Billboard/>
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies}/>
+        <MovieList title="My List" data={favorites}/>
       </div>
     </>
   )
