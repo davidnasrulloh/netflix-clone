57f720c735965645c7cffe8bec9347fc0715fda1
David Nasrulloh <davidkrb52@gmail.com>
2023-06-01 17:32:03 +0700

    fixing bug in favorite

--- hooks/useFavorites.ts
+++ hooks/useFavorites.ts
@@ -1,20 +1,18 @@
-import useSWR  from 'swr';
+import useSWR from 'swr';
 import fetcher from '@/libs/fetcher';
 
-
-const useFavorites = () => {
-    const {data, error, isLoading, mutate} = useSWR('/api/favorites', fetcher, {
+const useMovies = () => {
+    const { data, error, isLoading, mutate } = useSWR('/api/favorites', fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false,
-    })
-
+    });
     return {
-        data, 
+        data,
         error,
         isLoading,
         mutate
     }
-}
+};
 
-export default useFavorites;
+export default useMovies;
